3f1eab0b9ceeb08687896aca476eedbfde0f1e10
chaapatti <137924830+chaapatti@users.noreply.github.com>
2024-08-18 14:43:59 +0000

    code complete

--- main.py
+++ main.py
@@ -13,10 +13,37 @@ class Game:
         print(f"""_{self.p[0]}_|_{self.p[1]}_|_{self.p[2]}_
 _{self.p[3]}_|_{self.p[4]}_|_{self.p[5]}_
  {self.p[6]} | {self.p[7]} | {self.p[8]}""")
+        
+    def winning_cases(self, c):
+        if (self.p[0] == self.p[1] == self.p[2] == c):
+            return c  # these are the winning cases that we have to check for player.
+        elif (self.p[3] == self.p[4] == self.p[5] == c):
+            return c
+        elif (self.p[6] == self.p[7] == self.p[8] == c):
+            return c
+        elif (self.p[0] == self.p[3] == self.p[6] == c):
+            return c
+        elif (self.p[1] == self.p[4] == self.p[7] == c):
+            return c
+        elif (self.p[2] == self.p[5] == self.p[8] == c):
+            return c
+        elif (self.p[0] == self.p[4] == self.p[8] == c):
+            return c
+        elif (self.p[6] == self.p[4] == self.p[2] ==c):
+            return c
+        else:
+            return ""
+    
+    def switch_player(self, plyr):
+        if (plyr == "o"):
+            return "x"
+        else:
+            return "o"
+
 class Player:
     def __init__(self):
         self.character = ""
-
+        
 
 def game_loop():
     tictactoe = Game()
@@ -41,17 +68,36 @@ def game_loop():
 
     print(f"{player_one.character},  {player_two.character}")
 #implemting the character display on the grid
+    plyr = player_one.character
     while(game_play):
-        x = input("enter the position number to enter the mark [1-9]");
+        
+        
+        x = int(input("enter the position number to enter the mark [1-9]"))
         if (x == "q"):
             game_play = False
-        if(tictactoe.p[x] == " " or tictactoe.p[x] == "_"):
-            # here we will write a logic to decide whose turn is now. and based on that we will use the player.character
-            # to display the character in particular location.
-            # what can we do to track the turns.
-            # use a flag to indicate the turn or use the player character as an indication of turn
-            # lets say in a multiplayer game we can set the variable to the characters respective value. and use it to 
-            # display active character.
+        if (x > 9):
+            print("index out of reach")
+        else:
+            if(tictactoe.p[x] == " " or tictactoe.p[x] == "_"):
+                # here we will write a logic to decide whose turn is now. and based on that we will use the player.character
+                # to display the character in particular location.
+                # what can we do to track the turns.
+                # use a flag to indicate the turn or use the player character as an indication of turn
+                # lets say in a multiplayer game we can set the variable to the characters respective value. and use it to 
+                # display active character.
+                tictactoe.p[x]= plyr
+                winning_result = tictactoe.winning_cases(plyr)
+                if(winning_result == "o"):
+                    print("player one is winner")
+                    break
+                elif(winning_result == "x"):
+                    print("player two is winner")
+                    break
+                plyr = tictactoe.switch_player(plyr)
+                tictactoe.board_display()
+            else:
+                print("enter in the empty position.")
+            
 
 
 
